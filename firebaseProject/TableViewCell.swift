d5bdec77de35875db2a8470f8da8f6940e7bf0e6
KenYeh1023 <s1011451@gmail.com>
2019-08-19 02:16:02 +0800

    完成tableView介面UI

---
+++ firebaseProject/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  firebaseProject
+//
+//  Created by Ken Yeh on 2019/8/19.
+//  Copyright © 2019 Ken Yeh. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
